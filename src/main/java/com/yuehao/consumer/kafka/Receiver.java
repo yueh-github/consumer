affac3dfc69150ffafde8b05ba3614a373534ffb
yuehao <83541408@qq.com>
2017-08-28 16:07:07 +0800

    consumer

--- src/main/java/com/yuehao/consumer/kafka/Receiver.java
+++ src/main/java/com/yuehao/consumer/kafka/Receiver.java
@@ -2,10 +2,11 @@ package com.yuehao.consumer.kafka;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 /**
@@ -20,29 +21,42 @@ public class Receiver {
 
 
     //    @KafkaListener(topicPartitions = {@TopicPartition(partitions = {"0","1"}, topic = "test_01")})
-    @KafkaListener(topics = "test_01")
-    public void processMessage(String content, Acknowledgment ack) {
-        try {
-            Thread.sleep(2000);
-            Message message = gson.fromJson(content, Message.class);
-            logger.info("{}", gson.toJson(message));
-            ack.acknowledge();
-        } catch (Exception ex) {
+//    @KafkaListener(topics = "test_01")
+//    public void processMessage(String content, Acknowledgment ack) {
+//        try {
+//
+//            Thread.sleep(2000);
+//            Message message = gson.fromJson(content, Message.class);
+//            logger.info("{}", "接受到test_01的数据，开始解析...");
+//            logger.info("{}", gson.toJson(message));
+//            ack.acknowledge();
+//        } catch (Exception ex) {
+//
+//        }
+//    }
 
-        }
-    }
+//    @KafkaListener(topicPartitions = {@TopicPartition(partitions = {"0","1"}, topic = "test_01")})
+//    @KafkaListener(topics = "test_02")
+//    public void processMessageTest02(String content, Acknowledgment ack) {
+//        try {
+//            Thread.sleep(2000);
+//            Message message = gson.fromJson(content, Message.class);
+//            logger.info("{}", "接受到test_02的数据，开始解析...");
+//            logger.info("{}", gson.toJson(message));
+//            ack.acknowledge();
+//        } catch (Exception ex) {
+//
+//        }
+//    }
 
+    //    @KafkaListener(topicPartitions = {@TopicPartition(partitions = {"0", "1"}, topic = "test_01")})
+//    @KafkaListener(topicPartitions = {@TopicPartition(partitions = {"0", "1"}, topic = "test_02")})
 
-    //    @KafkaListener(topicPartitions = {@TopicPartition(partitions = {"1"}, topic = "test_01")})
-    @KafkaListener(topics = "test_02")
-    public void processMessageTest02(String content, Acknowledgment ack) {
-        try {
-            Thread.sleep(2000);
-            Message message = gson.fromJson(content, Message.class);
-            logger.info("{}", gson.toJson(message));
-            ack.acknowledge();
-        } catch (Exception ex) {
-
-        }
+    @KafkaListener(topicPartitions = {@TopicPartition(partitions = {"0", "1", "2", "3"}, topic = "test_02")}, topics = "test_02")
+//    @KafkaListener(topics = "test_02")
+    public void message(ConsumerRecord data) {
+        System.out.println(data.key());
+        System.out.println(data.partition());
+        System.out.println(gson.toJson(data.value()));
     }
 }
